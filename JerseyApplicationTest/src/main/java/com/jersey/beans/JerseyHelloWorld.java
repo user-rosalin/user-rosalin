0cdb6ecab5efc27da2c6216d7e76d73722917391
AVI <avinash.dabhade3727@gmail.com>
2020-02-15 20:45:56 +0530

    new method added

--- JerseyApplicationTest/src/main/java/com/jersey/beans/JerseyHelloWorld.java
+++ JerseyApplicationTest/src/main/java/com/jersey/beans/JerseyHelloWorld.java
@@ -35,5 +35,14 @@ public class JerseyHelloWorld {
 	        //Simply return the parameter passed as message
 	        return Response.status(200).entity(output).build();
 	    }
+	  @GET
+	  @Path("/{message1}")
+	    public Response getname18(@PathParam("message1") String msg)
+	    {
+	        String output = "Message requested : " + msg;
+	        //Simply return the parameter passed as message
+	        return Response.status(200).entity(output).build();
+	    }
+
 
 }
